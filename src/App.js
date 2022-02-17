da7a244e876a8f8251b79b9ca0d65509b18b88c2
Ylisdra <laura.morvan.fr@gmail.com>
2022-02-17 13:01:44 +0100

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import pokemons from './pokemons.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="Langage-selection">LangageSelection</button>
       </header>
+      <body>
+        <div className="CardList">
+          <div className="Card">
+            <img src="" className="Pokemon" alt="pokemon"/>
+            <p className="Type"></p>
+          </div>
+          <div className="Card">
+            <img src="" className="Pokemon" alt="pokemon"/>
+            <p className="Type"></p>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
